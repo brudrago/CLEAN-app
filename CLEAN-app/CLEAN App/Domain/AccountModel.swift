2df243a72bc323e8ab278f49e8bc6bd109b51a5a
Bru-Drago <bruna_drago@hotmail.com>
2021-01-02 16:37:43 -0300

    add AccountModel

---
+++ CLEAN-app/CLEAN App/Domain/AccountModel.swift
@@ -0,0 +1,14 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Bruna Fernanda Drago on 02/01/21.
+//
+import Foundation
+
+struct AccountModel {
+    var id      : String
+    var name    : String
+    var email   : String
+    var password: String
+}
---
+++ CLEAN-app/CLEAN App/Domain/AddAccount.swift
@@ -0,0 +1,20 @@
+//
+//  AddAccount.swift
+//  Domain
+//
+//  Created by Bruna Fernanda Drago on 02/01/21.
+//
+
+import Foundation
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result < AccountModel,Error>)->Void)
+}
+
+struct AddAccountModel {
+    var name                : String
+    var email               : String
+    var password            : String
+    var passwordConfirmation: String
+}
+
