47b9eb2a50635847da5b03e457193f0522a048b8
Bru-Drago <bruna_drago@hotmail.com>
2021-01-02 16:01:50 -0300

    iniciando o projeto

---
+++ CLEAN-app/CLEAN App/CLEAN App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLEAN App
+//
+//  Created by Bruna Fernanda Drago on 02/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
