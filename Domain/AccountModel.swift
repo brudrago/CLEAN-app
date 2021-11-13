6f800c8e10db89eb17fb0b80dc00b73a8ff0aa7b
Bruna Drago <bruna_drago@hotmail.com>
2021-11-13 16:41:31 -0300

    Create new project and add Domain layer

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,14 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Bruna Fernanda Drago on 13/11/21.
+//
+
+import Foundation
+
+struct AccountModel {
+    var name: String
+    var email: String
+    var password: String
+}
